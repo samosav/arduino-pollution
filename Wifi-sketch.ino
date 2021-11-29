4343c0673f47f0a473a86685e01ae548fab1649b
samosav <samosathegrandma@gmail.com>
2021-11-29 21:43:14 +0000

    First commit

---
+++ Wifi-sketch.ino
@@ -0,0 +1,40 @@
+#include <SPI.h>
+#include <WiFiNINA.h>
+
+char ssid[] = "TP-Link_92A4";          //  your network SSID (name)
+char pass[] = "41884805";   // your network password
+
+int status = WL_IDLE_STATUS;
+IPAddress server(74,125,115,105);  // Google
+
+// Initialize the client library
+WiFiClient client;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Attempting to connect to WPA network...");
+  Serial.print("SSID: ");
+  Serial.println(ssid);
+
+  status = WiFi.begin(ssid, pass);
+  if ( status != WL_CONNECTED) {
+    Serial.println("Couldn't get a wifi connection");
+    // don't do anything else:
+    while(true);
+  }
+  else {
+    Serial.println("Connected to wifi");
+    Serial.println("\nStarting connection...");
+    // if you get a connection, report back via serial:
+    if (client.connect(server, 80)) {
+      Serial.println("connected");
+      // Make a HTTP request:
+      client.println("GET /search?q=arduino HTTP/1.0");
+      client.println();
+    }
+  }
+}
+
+void loop() {
+
+}
